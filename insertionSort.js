3ac338014999b2c56897cc91e3c6d5f140d6f62a
Bruno Serra <bbucalonserra@hotmail.com>
2025-01-30 17:19:31 -0300

    Fixing errors in insertion sort algorithm

--- insertionSort.js
+++ insertionSort.js
@@ -1,21 +1,32 @@
-var arr = [
-    1, 2, 3, 4, 5, 6
-]
+var arr = [3, 2, 5, 1, 2];
 
-function Shift (array, i , j) {
+function Shift(array, i, j) {
     if (i <= j) {
         return array;
     }
 
     var store = array[i];
 
-    for (var k = 0; k <= i - k - 1; k++) {
-        array[i - k] = array[i - k - 1];
+    for (let k = i; k > j; k--) {
+        array[k] = array[k - 1];
     }
 
     array[j] = store;
 
-    return array
+    return array;
 }
 
-console.log(Shift(arr, 4, 2));
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let j = i;
+
+        while (j > 0 && array[j - 1] > array[i]) {
+            j--;
+        }
+
+        Shift(array, i, j); 
+    }
+    return array;
+}
+
+console.log(insertionSort(arr));
--- linearSearch.js
+++ linearSearch.js
@@ -1,6 +1,6 @@
 function linearSearch(arr, target) {
   for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === target) {
+      if (arr[i] == target) {
           return i; // Return the index of the found element
       }
   }
