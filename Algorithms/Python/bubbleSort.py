61d06f0edc3ac6a3b38e002239b33e95c79ce75c
Bruno Serra <bbucalonserra@hotmail.com>
2026-05-04 21:39:13 -0300

    Adjusting to print interaction from Bubble Sort.

--- Algorithms/Python/bubbleSort.py
+++ Algorithms/Python/bubbleSort.py
@@ -7,21 +7,25 @@ def swap(A, i, j):
     return A
 
 # Function Bubble Sort.
+# Set false if don't want to see the interactio step by step in terminal.
+print_interaction = True
+
 def BubbleSort(A, N):
     swapped = True
-    print("Var swapped to True.")
+    print("Var swapped to True.") if print_interaction == True else None
     while swapped:
-        print("While initiated.")
+        print("While initiated.") if print_interaction == True else None
         swapped = False
-        print("Var swapped to False.")
+        print("Var swapped to False.") if print_interaction == True else None
         for i in range(len(A) - 1):
-            print(f"interaction {i}")
+            print(f"Interaction {i}") if print_interaction == True else None
             if (A[i] > A[i+1]):
+                print(f"Array before swap: {A}")
                 swap(A, i, i+1)
-                print(f"Swapped {A[i]} with {A[i+1]}")
+                print(f"Swapped {A[i]} with {A[i+1]}, now array is A: {A}") if print_interaction == True else None
                 swapped = True
-                print("Var swapped to True.")
-        print("While ended.")
+                print("Var swapped to True.") if print_interaction == True else None
+        print("While ended.") if print_interaction == True else None
         N = N - 1
     
     return A
